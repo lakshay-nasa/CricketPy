48e76fb766492e23a8804acd65e4c05eeffc5361
lakshnasa <lakshay1717@gmail.com>
2021-01-02 23:36:43 +0530

    Created game base

---
+++ cricketgame.py
@@ -0,0 +1,24 @@
+#cricket game
+
+import random
+
+class comp:
+    pass
+    
+
+class Player:
+    def greet(self,name):
+        self.name = name
+        print("Welcome",self.name)
+
+# print(random.randint(1,10))
+
+# class Greet:
+
+user_name = (input("Enter your name: "))
+
+Player1 =  Player()
+
+Player1.greet(user_name)
+
+
