e21f0f448d8b3630d80decd9b6a32cb07e1b4883
lakshnasa <lakshay1717@gmail.com>
2021-01-03 23:51:58 +0530

    built game base

--- cricketgame.py
+++ cricketgame.py
@@ -2,33 +2,71 @@
 
 import random
 
-class comp:
+class Game_root:
+    pass
+
+
+class Comp(Game_root):
     def __init__(self,num):
         self.num = num
-        # print(self.num)
-        # self.level = level
-        # print("Your level", self.level)
+
     def randnum(self):
+        return self.num
+    
+    def __str__(self):
         print(self.num)
 
-class Player:
+class Player(Game_root):
     def __init__(self,name):
         self.name = name
         print("Welcome,",self.name)
 
-    
-# print(random.randint(1,10))
+    def choose_level(self,level = 1):
+        level = int(input("Choose your level--> 1 or 2 : "))
+        self.level = level
+        print(f"Level = {self.level}")
 
-# class Greet:
+    def game_start(self):
+        try:
+            input("Press Enter to continue...")
+        except SyntaxError:
+            print("Try again....****", input("Press Enter to continue...") , "***")
+
+        else:
+            print("Starting game...")
+            print(3)
+            print(2)
+            print(1)
+            print("GO")
+
+
 
 player_name = (input("Enter your name: "))
-# player_level = (int(input("choose your level: ")))
 
-random_number = random.randint(1,10)
-
-Comp1 = comp(random)
 Player1 =  Player(player_name)
+Player1.choose_level()
 
-# Player1.greet(user_name)
+if Player1.choose_level == 1:
+    random_number = random.randint(1,5)
+
+else:
+    random_number = random.randint(1,10)
+
+Comp1 = Comp(random_number)
+
+
+
+Player1.game_start()
+
+while True:
+    try:
+        player_show = int(input("Enter number 1 to 5 : "))
+    except Exception as e:
+        print("Enter valid number")
+
+    else:
+        print(f"Computer choosed -->{Comp1.randnum()}\n")
+        print(f"{player_name} choosed --> {player_show}")
+
 
 
